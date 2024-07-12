3bc2f5fb1868c16789b1ac21b90b1096da4e360e
abdellahsebti <abdellahsebti001@gmail.com>
2024-07-12 17:22:07 +0100

    update home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -113,7 +113,7 @@ function Home() {
 
 
                     </Link>
-                    <Link to="/contact">
+                    <Link to="#">
                         <button type='button' className='btn btn-outline-light btn-lg mx-0 mx-sm-2 my-2 my-sm-0'>Contact School</button>
                     </Link>
                 </div>
@@ -169,7 +169,7 @@ function Home() {
                     ))}
                 </div>
                 <Link to="/blog">
-                    <button type='button' className='btn btn-danger btn-lg mt-5' style={{ backgroundColor: '#3572EF', borderColor: '#3572EF' }}>Read More Blogs</button>
+                    <button type='button' className='btn btn-danger btn-lg mt-5' style={{ backgroundColor: '#3572EF', borderColor: '#3572EF' }}>Read More</button>
                 </Link>
             </div>
         </div>
